bf720db1264978ed752998306e4e3d719a122edc
yehuohan <yehuohan@qq.com>
2017-10-22 10:47:57 +0000

    style(cc-win.sh): add spaces

--- cc-win.sh
+++ cc-win.sh
@@ -14,12 +14,12 @@ if [ ! -d "../Vim" ]; then
     echo "No Vim."
     exit
 fi
-cp ../Vim/_vimrc ./cf-vim/.vimrc
+cp ../Vim/_vimrc                      ./cf-vim/.vimrc
 cp ../Vim/vimfiles/.ycm_extra_conf.py ./cf-vim/
-cp -r ../Vim/vimfiles/mySnippets/ ./cf-vim/
-cp -r ../Vim/vimfiles/autoload/*.vim ./cf-vim/autoload/
+cp -r ../Vim/vimfiles/mySnippets/     ./cf-vim/
+cp -r ../Vim/vimfiles/autoload/*.vim  ./cf-vim/autoload/
 
 # cf-msys2
-cp ~/.minttyrc ./cf-msys2/
+cp ~/.minttyrc  ./cf-msys2/
 cp ~/.gitconfig ./cf-msys2/
-cp ~/.zshrc ./cf-msys2/
+cp ~/.zshrc     ./cf-msys2/
