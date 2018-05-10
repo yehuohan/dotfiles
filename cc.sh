1fc74961168deeee1c88fdaabf38e72a65460f84
yehuohan <yehuohan@qq.com>
2018-05-10 00:55:09 +0000

    fix: replace xfce-power-manager with mate's and remove gestures conf

--- cc.sh
+++ cc.sh
@@ -35,7 +35,6 @@ if [ `uname` == "Linux" ]; then
         cp -r ../.config/i3         $cf_dir/.config/
         cp -r ../.config/i3status   $cf_dir/.config/
         cp -r ../.config/rofi       $cf_dir/.config/
-        cp -r ../.config/libinput-gestures.conf     $cf_dir/.config/
         # tmux
         cp ../.tmux.conf            $cf_dir/
         cp ../.tmux-status.conf     $cf_dir/
