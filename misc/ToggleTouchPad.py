d1bb208cd5c77673b89d06c63a5da34a603a015c
yehuohan <yehuohan@qq.com>
2018-07-14 13:55:04 +0000

    feat(misc): add on|off parameter for ToggleTouchPad.py

--- misc/ToggleTouchPad.py
+++ misc/ToggleTouchPad.py
@@ -2,12 +2,21 @@
 # -*- coding: utf-8 -*-
 
 """
-linked this file to /usr/local/bin with 'ln -s'
+Linked this file to /usr/local/bin with 'ln -s'
+Usage:
+    ./ToggleTouchPad.py         : toggle touchpad
+    ./ToggleTouchPad.py on      : enable touchpad
+    ./ToggleTouchPad.py off     : disable touchpad
 """
 
 import os
+import sys
 
-def check_touchpad_state(dev_num):
+def get_touchpad_state(dev_num):
+    """Get touchpad state
+    :Returns:
+        0 or 1
+    """
     dev_state = os.popen('xinput list-props  %s' % str(dev_num))
     for lines in dev_state.readlines():
         if 'Device Enabled' in lines:
@@ -16,8 +25,8 @@ def check_touchpad_state(dev_num):
             print ('signal now :%d' % signal)
             return signal
 
-
 def get_dev_num(dev_name = 'SynPS/2 Synaptics TouchPad'):
+    """Get touchpad device number"""
     dev_state = os.popen('xinput list')
     for lines in dev_state.readlines():
         if dev_name in lines:
@@ -29,24 +38,30 @@ def get_dev_num(dev_name = 'SynPS/2 Synaptics TouchPad'):
             return dev_num
     dev_state.close()
 
-def change_state(state_value, dev_num):
+def set_state(state_value, dev_num):
+    """Set touchpad state
+    :Parameters:
+        - state_value: 0 for disable touchpad and 1 for enable touchpad
+        - dev_num: touchpad device number
+    """
     print ('state_value = ', state_value)
-    state = not state_value
-    if state == True:
-        state = 1
-    #else:
-        #state = 0
-    #print ('state = ', state, str(state))
-    tem = os.popen("xinput set-prop %s 'Device Enabled' %s" % (str(dev_num), str(state)))
+    tem = os.popen("xinput set-prop %s 'Device Enabled' %s" % (str(dev_num), str(state_value)))
     tem.close()
 
-def main():
+def main(args):
     dev_num = get_dev_num()
-    state = check_touchpad_state(dev_num)
-    change_state(state, dev_num)
-
+    if len(args) >= 2:
+        if args[1] == 'on':
+            state = 1 
+        elif args[1] == 'off':
+            state = 0 
+    else:
+        state = get_touchpad_state(dev_num)
+        state = not state
+        state = 1 if state == True else 0
+    set_state(state, dev_num)
 
 if __name__ == '__main__':
-    main()
+    main(sys.argv)
 
 
