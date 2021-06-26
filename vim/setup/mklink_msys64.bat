adbb1ffa0745696b9fb0ad22aad3be92fe19b116
yehuohan <yehuohan@qq.com>
2021-06-26 16:50:07 +0800

    vim/setup: add mklink script for msys64

---
+++ vim/setup/mklink_msys64.bat
@@ -0,0 +1,6 @@
+mklink /D %APPS_HOME%\\msys64\\home\\%USERNAME%\\.vim\\autoload     %APPS_HOME%\\dotvim\\autoload
+mklink /D %APPS_HOME%\\msys64\\home\\%USERNAME%\\.vim\\bundle       %APPS_HOME%\\dotvim\\bundle
+mklink /D %APPS_HOME%\\msys64\\home\\%USERNAME%\\.vim\\pythonx      %APPS_HOME%\\dotvim\\pythonx
+mklink /D %APPS_HOME%\\msys64\\home\\%USERNAME%\\.vim\\snips        %APPS_HOME%\\dotvim\\snips
+mklink    %APPS_HOME%\\msys64\\home\\%USERNAME%\\.vim\\.init.vim    %APPS_HOME%\\dotvim\\.init.vim
+pause
