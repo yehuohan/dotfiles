3bdfc26f030a41e9f69937572115162a9efb9ac5
yehuohan <yehuohan@qq.com>
2021-08-09 23:12:04 +0800

    vim: expand dotvim of init.lua

--- vim/nvim/init.lua
+++ vim/nvim/init.lua
@@ -4,6 +4,7 @@ if vim.fn.has('win32') == 1 or vim.fn.has('win64') == 1 then
 else
     dotvim = '~/.vim'
 end
+dotvim = vim.fn.resolve(vim.fn.expand(dotvim))
 vim.opt.rtp:prepend(dotvim)
 vim.opt.packpath:prepend(dotvim)
 require('v').setup(dotvim)
