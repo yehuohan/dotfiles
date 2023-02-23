5ef725eafa6deb0ed9efc18db1532864886e7525
yehuohan <yehuohan@qq.com>
2023-02-23 23:26:27 +0800

    vim: update setup_link_msys64.bat

--- vim/setup/setup_link_msys64.bat
+++ vim/setup/setup_link_msys64.bat
@@ -4,9 +4,9 @@ mklink    %APPS_HOME%\msys64\home\%USERNAME%\.vim\.init.vim    %APPS_HOME%\dotvi
 mklink /D %APPS_HOME%\msys64\home\%USERNAME%\.vim\autoload     %APPS_HOME%\dotvim\autoload
 mklink /D %APPS_HOME%\msys64\home\%USERNAME%\.vim\viml         %APPS_HOME%\dotvim\viml
 mklink /D %APPS_HOME%\msys64\home\%USERNAME%\.vim\bundle       %APPS_HOME%\dotvim\bundle
-mklink /D %APPS_HOME%\msys64\home\%USERNAME%\.vim\pythonx      %APPS_HOME%\dotvim\pythonx
-mklink /D %APPS_HOME%\msys64\home\%USERNAME%\.vim\rplugin      %APPS_HOME%\dotvim\rplugin
 mklink /D %APPS_HOME%\msys64\home\%USERNAME%\.vim\snips        %APPS_HOME%\dotvim\snips
+REM mklink /D %APPS_HOME%\msys64\home\%USERNAME%\.vim\pythonx      %APPS_HOME%\dotvim\pythonx
+REM mklink /D %APPS_HOME%\msys64\home\%USERNAME%\.vim\rplugin      %APPS_HOME%\dotvim\rplugin
 
 echo Dotvim link was made successful!
 pause
