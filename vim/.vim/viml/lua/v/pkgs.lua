e7ece11fdf35c068bf8693a6718d693c2e3fda48
yehuohan <yehuohan@qq.com>
2023-03-18 09:59:58 +0800

    vim: update pkg_treesitter

--- vim/.vim/viml/lua/v/pkgs.lua
+++ vim/.vim/viml/lua/v/pkgs.lua
@@ -541,59 +541,57 @@ end
 
 -- 语法树
 local function pkg_treesitter()
-    if use.nts then
-        if vim.fn.empty(use.xgit) == 0 then
-            for _, c in pairs(require('nvim-treesitter.parsers').get_parser_configs()) do
-                c.install_info.url = c.install_info.url:gsub('https://github.com', use.xgit)
-            end
+    if vim.fn.empty(use.xgit) == 0 then
+        for _, c in pairs(require('nvim-treesitter.parsers').get_parser_configs()) do
+            c.install_info.url = c.install_info.url:gsub('https://github.com', use.xgit)
         end
-        local parser_dir = vim.env.DotVimLocal .. '/.treesitter'
-        require('nvim-treesitter.configs').setup({
-            parser_install_dir = parser_dir,
-            --ensure_installed = { 'c', 'cpp', 'rust', 'vim', 'lua', 'python', 'markdown', 'markdown_inline', },
-            --auto_install = true,
-            highlight = {
-                enable = true,
-                additional_vim_regex_highlighting = false,
-            },
-            indent = {
-                enable = true,
-                disable = { 'python' },
-            },
-            incremental_selection = {
-                enable = true,
-                keymaps = {
-                    init_selection = '<M-g>',
-                    node_incremental = '<M-g>',
-                    node_decremental = '<M-a>',
-                    scope_incremental = '<M-v>',
-                },
-            },
-            rainbow = {
-                enable = true,
-                extended_mode = true,
-                max_file_lines = nil,
-            },
-        })
-        vim.opt.runtimepath:append(parser_dir)
-        m.nnore({
-            '<leader>sh',
-            function()
-                vim.cmd([[TSBufToggle highlight]])
-                local buf = vim.api.nvim_get_current_buf()
-                local res = require('vim.treesitter.highlighter').active[buf]
-                vim.notify('Treesitter highlight is ' .. (res and 'enabled' or 'disabled'))
-            end,
-        })
-        m.nnore({
-            '<leader>si',
-            function()
-                vim.cmd([[TSBufToggle indent]])
-                local res = vim.bo.indentexpr == 'nvim_treesitter#indent()'
-                vim.notify('Treesitter indent is ' .. (res and 'enabled' or 'disabled'))
-            end,
-        })
     end
+    local parser_dir = vim.env.DotVimLocal .. '/.treesitter'
+    require('nvim-treesitter.configs').setup({
+        parser_install_dir = parser_dir,
+        --ensure_installed = { 'c', 'cpp', 'rust', 'vim', 'lua', 'python', 'markdown', 'markdown_inline', },
+        --auto_install = true,
+        highlight = {
+            enable = true,
+            additional_vim_regex_highlighting = false,
+        },
+        indent = {
+            enable = true,
+            disable = { 'python' },
+        },
+        incremental_selection = {
+            enable = true,
+            keymaps = {
+                init_selection = '<M-g>',
+                node_incremental = '<M-g>',
+                node_decremental = '<M-a>',
+                scope_incremental = '<M-v>',
+            },
+        },
+        rainbow = {
+            enable = true,
+            extended_mode = true,
+            max_file_lines = nil,
+        },
+    })
+    vim.opt.runtimepath:append(parser_dir)
+    m.nnore({
+        '<leader>sh',
+        function()
+            vim.cmd([[TSBufToggle highlight]])
+            local buf = vim.api.nvim_get_current_buf()
+            local res = require('vim.treesitter.highlighter').active[buf]
+            vim.notify('Treesitter highlight is ' .. (res and 'enabled' or 'disabled'))
+        end,
+    })
+    m.nnore({
+        '<leader>si',
+        function()
+            vim.cmd([[TSBufToggle indent]])
+            local res = vim.bo.indentexpr == 'nvim_treesitter#indent()'
+            vim.notify('Treesitter indent is ' .. (res and 'enabled' or 'disabled'))
+        end,
+    })
 end
 
 --------------------------------------------------------------------------------
