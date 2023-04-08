2d289e051c9990a5f43bbe8a65562556ebf7ea64
yehuohan <yehuohan@qq.com>
2023-04-08 13:42:17 +0800

    vim: remove env.lua, use.lua, use.lua, rename .init.lua to init.lua

---
+++ vim/.vim/viml/lua/v/init.lua
@@ -0,0 +1,29 @@
+--~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+-- init.lua: configuration for neovim.
+-- Github: https://github.com/yehuohan/dotconfigs
+-- Author: yehuohan, <yehuohan@qq.com>, <yehuohan@gmail.com>
+--~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+function IsLinux()
+    return (vim.fn.has('unix') == 1)
+        and (vim.fn.has('macunix') == 0)
+        and (vim.fn.has('win32unix') == 0)
+end
+
+function IsWin()
+    return (vim.fn.has('win32') == 1) or (vim.fn.has('win64') == 1)
+end
+
+function IsGw()
+    return vim.fn.has('win32unix') == 1
+end
+
+function IsMac()
+    return vim.fn.has('mac') == 1
+end
+
+return {
+    setup = function()
+        require('v.pkgs').setup()
+    end,
+}
