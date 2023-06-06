90e7727fe6772359bea5eff9176dfb43fccbd356
yehuohan <yehuohan@qq.com>
2023-06-06 23:25:40 +0800

    vim: update setup_link_msys64.bat

--- vim/setup/setup_link_msys64.bat
+++ vim/setup/setup_link_msys64.bat
@@ -5,6 +5,7 @@ for %%i in ("%~dp0\..\") do set DIR_VIM=%%~dpi
 echo DIR_VIM: %DIR_VIM%
 
 copy %DIR_VIM%\gvim\.vimrc %APPS_HOME%\msys64\home\%USERNAME%\
+md %APPS_HOME%\msys64\home\%USERNAME%\dotvim
 mklink /D %APPS_HOME%\msys64\home\%USERNAME%\dotvim\autoload    %APPS_HOME%\dotvim\autoload
 mklink /D %APPS_HOME%\msys64\home\%USERNAME%\dotvim\bundle      %APPS_HOME%\dotvim\bundle
 mklink /D %APPS_HOME%\msys64\home\%USERNAME%\dotvim\init        %APPS_HOME%\dotvim\init
