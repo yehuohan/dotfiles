c22569c98383c876b19ecdf599059d75c9989cf8
yehuohan <yehuohan@qq.com>
2023-06-19 13:40:21 +0800

    disk: update comments

--- disk/setup/setup_disk.sh
+++ disk/setup/setup_disk.sh
@@ -3,13 +3,13 @@
 DIR_DISK=$(readlink -f "$(dirname "$0")/../")
 echo DIR_DISK: $DIR_DISK
 
-# set zsh url
+# Set zsh url
 ZSH_URL=githubusercontent
 if [[ ! -z "$1" ]]; then
     ZSH_URL=$1
 fi
 
-# install zsh and git
+# Install zsh and git
 if [[ ! -x "$(command -v zsh)" ]]; then
     pacman -S zsh
 fi
@@ -17,7 +17,7 @@ if [[ ! -x "$(command -v git)" ]]; then
     pacman -S git
 fi
 
-# install on-my-zsh from https://github.com/ohmyzsh/ohmyzsh
+# Install on-my-zsh from https://github.com/ohmyzsh/ohmyzsh
 if [[ ! -d "$HOME/.oh-my-zsh" ]]; then
     sh -c "$(wget -O- https://raw.${ZSH_URL}.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 fi
