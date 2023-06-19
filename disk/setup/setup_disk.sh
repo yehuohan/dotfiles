9db32bead40d9d1d3bf58236b6cf8a92d50125a5
yehuohan <yehuohan@qq.com>
2023-06-19 13:03:21 +0800

    disk: add setup_disk.sh

---
+++ disk/setup/setup_disk.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+DIR_DISK=$(readlink -f "$(dirname "$0")/../")
+echo DIR_DISK: $DIR_DISK
+
+# set zsh url
+ZSH_URL=githubusercontent
+if [[ ! -z "$1" ]]; then
+    ZSH_URL=$1
+fi
+
+# install zsh and git
+if [[ ! -x "$(command -v zsh)" ]]; then
+    pacman -S zsh
+fi
+if [[ ! -x "$(command -v git)" ]]; then
+    pacman -S git
+fi
+
+# install on-my-zsh from https://github.com/ohmyzsh/ohmyzsh
+if [[ ! -d "$HOME/.oh-my-zsh" ]]; then
+    sh -c "$(wget -O- https://raw.${ZSH_URL}.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+fi
+
+cp -r $DIR_DISK/msys2/etc/*         /etc/
+cp -r $DIR_DISK/msys2/etc/.*        /etc/
+cp -r $DIR_DISK/msys2/home/*        ~/
+cp -r $DIR_DISK/msys2/home/.*       ~/
+cp -r $DIR_DISK/home-win/*          $USERPROFILE/
+cp -r $DIR_DISK/home-win/.*         $USERPROFILE/
+
+echo "Disk setup was completed!"
