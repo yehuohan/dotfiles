da312aae1ce191fe3708d4cb7b4d5ef8959f1736
yehuohan <yehuohan@qq.com>
2023-09-07 23:11:41 +0800

    disk: udpate scoop patch

--- disk/setup/setup_scoop.ps1
+++ disk/setup/setup_scoop.ps1
@@ -1,41 +1,46 @@
 
-# Get install.ps1 from https://github.com/ScoopInstaller/Install/blob/master/install.ps1
+#.Install
+<#
+Get install.ps1 from https://github.com/ScoopInstaller/Install/blob/master/install.ps1
+Then unlock install.ps1 from attributes to install scoop. How stupid!
+#>
 Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
 irm get.scoop.sh -outfile 'install.ps1'
+.\install.ps1 -ScoopDir 'C:\apps\_packs' -ScoopGlobalDir 'C:\apps\_packs' -NoProxy
+.\install.ps1 -ScoopDir "$env:APPS_HOME\_packs" -ScoopGlobalDir "$env:APPS_HOME\_packs" -NoProxy
 
-# Patch repos
+#.Patch
 $SCOOP_PACKAGE_REPO = "https://kgithub.com/ScoopInstaller/Scoop/archive/master.zip"
 $SCOOP_MAIN_BUCKET_REPO = "https://kgithub.com/ScoopInstaller/Main/archive/master.zip"
 $SCOOP_PACKAGE_GIT_REPO = "https://kgithub.com/ScoopInstaller/Scoop.git"
 $SCOOP_MAIN_BUCKET_GIT_REPO = "https://kgithub.com/ScoopInstaller/Main.git"
-
-# Install scoop (Unlock install.ps1 from attributes. How stupid!)
-.\install.ps1 -ScoopDir 'C:\apps\_packs' -ScoopGlobalDir 'C:\apps\_packs' -NoProxy
 .\install.ps1 -ScoopDir "$env:APPS_HOME\_packs" -ScoopGlobalDir "$env:APPS_HOME\_packs" -NoProxy
-
-# Patch scoop
-# $APPS_HOME/_packs/apps/scoop/current/lib/install.ps1
-# $APPS_HOME/_packs/apps/scoop/current/libexec/scoop-download.ps1
-#   => before invoking Invoke-CachedDownload
+<#
+Patch scoop source code after install:
+    $APPS_HOME/_packs/apps/scoop/current/lib/install.ps1
+    $APPS_HOME/_packs/apps/scoop/current/libexec/scoop-download.ps1
+    $APPS_HOME/_packs/apps/scoop/current/libexec/scoop-update.ps1
+        => before invoking Invoke-CachedDownload
+#>
 +$url_patch = "$url".Replace("https://github.com", "https://kgithub.com").Replace("https://raw.githubusercontent.com", "https://raw.kgithub.com")
--Invoke-CachedDownload $app $version $url "$dir\$fname" $cookies $use_cache
-+Invoke-CachedDownload $app $version $url_patch "$dir\$fname" $cookies $use_cache
-
-# Config scoop
+-Invoke-CachedDownload $app $version $url ...
++Invoke-CachedDownload $app $version $url_patch ...
+<# Code patch is not compatible with aria2 #>
 scoop config scoop_repo http://kgithub.com/ScoopInstaller/Scoop
 scoop config aria2-enabled false
 
-# Add buckets
-# 'extras' is some big, so can clone manually
-# cd $APPS_HOME/_packs/buckets
-# git clone --depth 1 https://kgithub.com/ScoopInstaller/Extras.git
-scoop bucket add extras https://kgithub.com/ScoopInstaller/Extras.git
-scoop bucket add versions https://kgithub.com/ScoopInstaller/Versions.git
-scoop bucket add nonportable https://kgithub.com/ScoopInstaller/Nonportable.git
-scoop bucket add nerd-fonts https://kgithub.com/matthewjberger/scoop-nerd-fonts.git
+#.Bucket
+<#
+The 'extras' is some big, so can clone manually or use mirror github:
+    cd $APPS_HOME/_packs/buckets
+    git clone --depth 1 https://github.com/ScoopInstaller/Extras.git
+#>
+scoop bucket add extras https://github.com/ScoopInstaller/Extras.git
+scoop bucket add versions https://github.com/ScoopInstaller/Versions.git
+scoop bucket add nonportable https://github.com/ScoopInstaller/Nonportable.git
+scoop bucket add nerd-fonts https://github.com/matthewjberger/scoop-nerd-fonts.git
 
-
-# Add packs
+#.Add
 scoop install neovim neovide nvy vim-nightly -a 64bit
 scoop install bat fd fzf ripgrep lf universal-ctags
 scoop install 7zip cmake ninja
@@ -46,9 +51,7 @@ scoop install notepadnext honeyview qttabbar-indiff-np
 scoop download clink -h
 scoop install CodeNewRoman-NF-Mono FantasqueSansMono-NF-Mono -s
 
-# Add optional packs
 scoop install vulkan renderdoc shadered # Then run apps/vulkan/current/install-vk-layers.ps1
 scoop install cuda11.1 # Prefer conda install cudatoolkit cudnn
 scoop install llvm install portable-virtualbox
 scoop install draw.io wpsoffice sioyek
-
