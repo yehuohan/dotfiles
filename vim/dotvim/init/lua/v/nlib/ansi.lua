723ff53dda0ce04df494b3a38eedf6265d763c92
yehuohan <yehuohan@qq.com>
2024-12-20 01:39:46 +0800

    vim: add new_sgr

--- vim/dotvim/init/lua/v/nlib/ansi.lua
+++ vim/dotvim/init/lua/v/nlib/ansi.lua
@@ -9,14 +9,11 @@
 local CSI_PAT = '\x1b%[([%d:;<=>%?]*)([CHKm])'
 
 --- Basic SGR color codes
---- * bold = 1
---- * italic = 3
---- * underline = 4
 --- * fg = 30~37, 38, 90~97
 --- * bg = 40~47, 48, 100~107
 local CSI_SGR = {
     hls = {},
-    colors = {
+    fgcolors = {
         ['30'] = '#1e1e1e', --'#000000',
         ['31'] = '#cc241d', --'#800000',
         ['32'] = '#98971a', --'#008000',
@@ -33,6 +30,24 @@ local CSI_SGR = {
         ['95'] = '#d3869b', --'#ff00ff',
         ['96'] = '#8ec07c', --'#00ffff',
         ['97'] = '#ebdbb2', --'#ffffff',
+    },
+    bgcolors = {
+        ['40'] = '#1e1e1e', --'#000000',
+        ['41'] = '#cc241d', --'#800000',
+        ['42'] = '#98971a', --'#008000',
+        ['43'] = '#d79921', --'#808000',
+        ['44'] = '#458588', --'#000080',
+        ['45'] = '#b16286', --'#800080',
+        ['46'] = '#689d6a', --'#008080',
+        ['47'] = '#ebdbb2', --'#c0c0c0',
+        ['100'] = '#928374', --'#808080',
+        ['101'] = '#fb4934', --'#ff0000',
+        ['102'] = '#b8bb26', --'#00ff00',
+        ['103'] = '#fabd2f', --'#ffff00',
+        ['104'] = '#83a598', --'#0000ff',
+        ['105'] = '#d3869b', --'#ff00ff',
+        ['106'] = '#8ec07c', --'#00ffff',
+        ['107'] = '#ebdbb2', --'#ffffff',
     },
 }
 
@@ -72,34 +87,90 @@ local function next_csi(str, pat)
     end
 end
 
---- Generate highlight from SGR
-local function sgr2hl(curhl, args)
-    if args == '' or args == '0' then
-        return nil
-    end
+local function new_sgr()
+    local opts = {
+        fg = nil,
+        bg = nil,
+        bold = false,
+        italic = false,
+        underline = false,
+        strikethrough = false,
+    }
+    local sgr = { hl = nil }
 
-    -- Get color code
-    local c = args:match('38;5;(%d*)')
-    if c then
-        local n = tonumber(c)
-        c = n <= 7 and tostring(30 + n) or tostring(90 + n - 8)
-    else
-        c = args
-    end
+    sgr.tohl = function(args)
+        if args == '' or args == '0' then
+            sgr.hl = nil
+            opts.fg = nil
+            opts.bg = nil
+            opts.bold = false
+            opts.italic = false
+            opts.underline = false
+            opts.strikethrough = false
+            return
+        end
+        if args == '1' then
+            opts.bold = true
+        elseif args == '21' then
+            opts.bold = false
+        elseif args == '3' then
+            opts.italic = true
+        elseif args == '23' then
+            opts.italic = false
+        elseif args == '4' then
+            opts.underline = true
+        elseif args == '24' then
+            opts.underline = false
+        elseif args == '9' then
+            opts.strikethrough = true
+        elseif args == '29' then
+            opts.strikethrough = false
+        end
 
-    -- Only support simple foreground color
-    local hl = nil
-    if CSI_SGR.colors[c] then
-        if CSI_SGR.hls[c] then
-            hl = CSI_SGR.hls[c]
+        -- Get foreground color code
+        local fc = args:match('38;5;(%d*)')
+        if fc then
+            local n = tonumber(fc)
+            fc = n <= 7 and tostring(30 + n) or tostring(90 + n - 8)
         else
-            local fg = CSI_SGR.colors[c]
-            hl = 'v.nlib.ansi.fg' .. c
-            vim.api.nvim_set_hl(0, hl, { fg = fg })
-            CSI_SGR.hls[c] = hl
+            fc = args
+        end
+        if CSI_SGR.fgcolors[fc] then
+            opts.fg = CSI_SGR.fgcolors[fc]
+        end
+
+        -- Get background color code
+        local bc = args:match('48;5;(%d*)')
+        if bc then
+            local n = tonumber(bc)
+            bc = n <= 7 and tostring(30 + n) or tostring(90 + n - 8)
+        else
+            bc = args
+        end
+        if CSI_SGR.bgcolors[bc] then
+            opts.bg = CSI_SGR.bgcolors[bc]
+        end
+
+        -- Create highlights
+        local key = '.'
+            .. (opts.fg and string.sub(opts.fg, 2) or 'n')
+            .. '.'
+            .. (opts.bg and string.sub(opts.bg, 2) or 'n')
+            .. '.'
+            .. (opts.bold and '1' or '0')
+            .. (opts.italic and '1' or '0')
+            .. (opts.underline and '1' or '0')
+            .. (opts.strikethrough and '1' or '0')
+        if CSI_SGR.hls[key] then
+            sgr.hl = CSI_SGR.hls[key]
+        else
+            sgr.hl = 'v.nlib.ansi' .. key
+            vim.api.nvim_set_hl(0, sgr.hl, opts)
+            CSI_SGR.hls[key] = sgr.hl
         end
     end
-    return hl or curhl
+
+    return sgr
 end
 
 local function new()
@@ -108,7 +179,7 @@ local function new()
     local srow = 1 -- Cursor position row
     -- local scol = 1 -- Cursor position col
     local erased_lines = 0 -- Erased lines should be displayed too
-    local hlgrp = nil -- Cursor highlight group from SGR
+    local sgr = new_sgr()
 
     -- ANSI object
     local ansi = {}
@@ -123,7 +194,7 @@ local function new()
         srow = 1
         -- scol = 1
         erased_lines = 0
-        hlgrp = nil
+        sgr = new_sgr()
     end
 
     ansi.feed = function(linestr, is_pending, verbose)
@@ -145,8 +216,8 @@ local function new()
                     local s_hl = hlts[srow] or {}
                     local cs = string.len(s_line) -- col_start
                     local ce = cs + string.len(trimed) -- col_end
-                    if hlgrp then
-                        s_hl[#s_hl + 1] = { hlgrp, srow, cs, ce }
+                    if sgr.hl then
+                        s_hl[#s_hl + 1] = { sgr.hl, srow, cs, ce }
                         if verb_h then
                             trimed = trimed .. ('<%d,%d,%d>'):format(srow, cs, ce)
                         end
@@ -201,7 +272,7 @@ local function new()
                     bufs[srow] = string.rep(verb_n and '%' or ' ', cur_col)
                 end
             elseif byte == 'm' then
-                hlgrp = sgr2hl(hlgrp, args)
+                sgr.tohl(args)
             end
         end
 
