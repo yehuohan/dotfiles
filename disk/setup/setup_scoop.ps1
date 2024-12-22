e5a0b84884d298612bdbbbd412bc06debe1818c5
yehuohan <yehuohan@qq.com>
2024-12-22 15:21:37 +0800

    disk,root: update rust cargo config

--- disk/setup/setup_scoop.ps1
+++ disk/setup/setup_scoop.ps1
@@ -59,6 +59,6 @@ scoop install renderdoc shadered
 scoop install cuda11.1 # Prefer conda install cudatoolkit cudnn
 scoop install llvm portable-virtualbox
 
-scoop install typst notepadnext honeyview potplayer
-scoop install draw.io wpsoffice sioyek
+scoop install notepadnext typst draw.io wpsoffice
+scoop install honeyview potplayer sioyek
 scoop shim add sioyek $env:DOT_APPS/_packs/apps/sioyek/current/sioyek.exe
