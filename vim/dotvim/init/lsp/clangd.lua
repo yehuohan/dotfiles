e3006859351f30d420776634a12e34bb7abd574d
yehuohan <yehuohan@qq.com>
2026-01-23 00:58:31 +0800

    vim: set offset_encoding = "utf-8" for clangd

--- vim/dotvim/init/lsp/clangd.lua
+++ vim/dotvim/init/lsp/clangd.lua
@@ -2,6 +2,7 @@ return {
 	-- Treesitter is better than lsp's semantic
 	on_init = function(client)
 		client.server_capabilities.semanticTokensProvider = nil
+		client.offset_encoding = "utf-8"
 	end,
 	cmd = { "clangd", "--header-insertion=never" },
 }
